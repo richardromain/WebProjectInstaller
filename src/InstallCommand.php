15d58e76b8bb35793d79f6b165a1efb58400a9ed
Romain Richard <romain.richard.19@gmail.com>
2017-02-25 01:23:17 +0100

    WIP : Choice for the good framework in command line

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -3,10 +3,10 @@ namespace WebProjectInstaller\Console;
 use RuntimeException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 class InstallCommand extends Command
 {
     /**
@@ -33,7 +33,24 @@ class InstallCommand extends Command
         $this->verifyApplicationDoesntExist(
             $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd()
         );
-        $output->writeln('<info>Crafting application...</info>');
+
+        $output->writeln('<info>Welcome to the web project installer.</info>');
+
+        $helper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            '<question>Please choose your framework backend</question>',
+            ['laravel', 'symfony']
+        );
+        $framework = $helper->ask($input, $output, $question);
+        $output->writeln('You have just selected: ' . $framework);
+
+        $process = new Process($this->getCommandInstallFramework($framework, $input->getArgument('name')), $directory, null, null, null);
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            $process->setTty(true);
+        }
+        $process->run(function ($type, $line) use ($output) {
+            $output->write($line);
+        });
     }
 
     /**
@@ -54,11 +71,23 @@ class InstallCommand extends Command
      *
      * @return string
      */
-    protected function findComposer()
+    private function findComposer()
     {
         if (file_exists(getcwd().'/composer.phar')) {
             return '"'.PHP_BINARY.'" composer.phar';
         }
         return 'composer';
+    }
+
+    private function getCommandInstallFramework($framework, $name)
+    {
+        $composer = $this->findComposer();
+        switch ($framework) {
+            case 'laravel':
+                return $composer.' create-project laravel/laravel --prefer-dist '.$name;
+                break;
+            case 'symfony':
+                return $composer.' create-project symfony/framework-standard-edition '.$name;
+        }
     }
 }
